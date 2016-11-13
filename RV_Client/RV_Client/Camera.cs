cb59adbd5530ec1cfdc26067e5f9bbac1c557d9d
natylkasmile <natylkasmile@gmail.com>
2016-11-13 20:37:12 +0400

    Add files via upload
    ckient

---
+++ RV_Client/RV_Client/Camera.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace RV_Client
+{
+    class Camera
+    {
+        float av_speed = 0;
+        int count = 0;
+        const int slep = 50;
+        int sl = 0;
+        public int name;
+
+        public Camera(int _name)
+        {
+            name = _name;
+        }
+        public void Survey()
+        {
+
+        }
+        public float Speed_Fixing()
+        {
+            Random r = new Random();
+            Thread.Sleep(10 * slep);
+            av_speed = r.Next(10, 150);
+            return av_speed;
+        }
+        public int Count_Fixing()
+        {
+            Random r = new Random();
+            Thread.Sleep(10 * slep);
+            count = r.Next(1, 200);
+            return count;
+        }
+        public void Send(int c, float sp)
+        {
+            Random r = new Random();
+            sl = r.Next(10, 100);
+            Thread.Sleep(sl * slep);
+           // Get_info(sp, c);
+        }
+    }
+}
---
+++ RV_Client/RV_Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace RV_Client
+{
+    class Program
+    {
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        const int MaxCountCam = 2;
+        const int MaxCountTrans = 10;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i < MaxCountTrans; i++)
+                {
+                    IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    // подключаемся к удаленному хосту
+                    socket.Connect(ipPoint);
+
+
+                    Camera cam = new Camera(1);
+                    int count; float sp;
+                    sp = cam.Speed_Fixing();
+                    count = cam.Count_Fixing();
+                    Console.WriteLine("Камера" + cam.name.ToString() + " отправляет данные: Средняя скорость " + sp.ToString() + " , количество машин " + count.ToString());
+
+                    string message = sp.ToString() + " " +count.ToString();
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    socket.Send(data);
+
+                    // получаем ответ
+                    data = new byte[256]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+                    Console.WriteLine("ответ сервера: " + builder.ToString());
+
+                    // закрываем сокет
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+
+    }
+}
